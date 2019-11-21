616c4be2924ed20ad7bd6e05984b23df6afbf698
Fernando Palazuelo <fpalazuelo@gmail.com>
2019-11-21 04:39:02 +0100

    pequeñas gestiones con los drawers

--- AppAndroidDetectorCaidas/app/src/main/java/com/detectorcaidas/MainActivity.java
+++ AppAndroidDetectorCaidas/app/src/main/java/com/detectorcaidas/MainActivity.java
@@ -1,21 +1,62 @@
 package com.detectorcaidas;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
-import android.widget.TextView;
+import androidx.wear.widget.drawer.WearableNavigationDrawerView.WearableNavigationDrawerAdapter;
 
-public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+import androidx.wear.widget.drawer.WearableNavigationDrawerView;
 
+public class MainActivity extends WearableActivity implements WearableNavigationDrawerView.OnItemSelectedListener {
+
+    private static final String TAG = "MainActivity";
+
+    private WearableNavigationDrawerView top_navigation_drawer;
+    private String[] arrayViews = {"Inicio","Ajustes"} ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        top_navigation_drawer = findViewById(R.id.top_navigation_drawer);
+        top_navigation_drawer.setAdapter(new NavigationAdapter(this));
 
+
+        top_navigation_drawer.getController().peekDrawer();
+        top_navigation_drawer.addOnItemSelectedListener(this);
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+
+    @Override
+    public void onItemSelected(int pos) {
+
+    }
+
+    private final class NavigationAdapter extends WearableNavigationDrawerAdapter {
+
+        private final Context context;
+
+        NavigationAdapter(Context context){
+            this.context = context;
+        }
+
+        @Override
+        public CharSequence getItemText(int pos) {
+            return arrayViews[pos];
+        }
+
+        @Override
+        public Drawable getItemDrawable(int pos) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return arrayViews.length;
+        }
     }
 }
